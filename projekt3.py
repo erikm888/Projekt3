ee3ed0f86091aeaf3258d07192db017a68129b4e
Erik Milosovicí <erik.milosovicl@gmail.com>
2023-01-28 19:13:07 +0100

    change of names

--- projekt3.py
+++ projekt3.py
@@ -16,12 +16,12 @@ def makeSoup(web):
     return BeautifulSoup(get(web).text, features="html.parser")
 
 
-def okresy(web, writer):
+def districts(web, writer):
     table, head = [], []
     soup = makeSoup(web)
-    okresy = soup.find_all("tr")
-    for okres in okresy:
-        cell = okres.find_all("td")
+    districts = soup.find_all("tr")
+    for district in districts:
+        cell = district.find_all("td")
         if cell and cell[0].text != "-":
             if not head:
                 head = header(urljoin(web, cell[2].a["href"]))
@@ -29,20 +29,20 @@ def okresy(web, writer):
                     writer.writerow(head)
             code = int(cell[0].text)
             location = cell[1].text
-            volici, vydane, platne, strany = obec(
+            voters, released, valid, parties = town(
                 urljoin(web, cell[2].a["href"]))
-            table = [code, location, volici, vydane, platne]
-            table.extend(strany)
+            table = [code, location, voters, released, valid]
+            table.extend(parties)
             writer.writerow(table)
     return table, head
 
 
-def obec(web):
-    volici = extract(web, "sa2")
-    vydane = extract(web, "sa3")
-    platne = extract(web, "sa6")
-    strany = extract_stran(web)
-    return volici, vydane, platne, strany
+def town(web):
+    voters = extract(web, "sa2")
+    published = extract(web, "sa3")
+    valid = extract(web, "sa6")
+    parties = extract_parties(web)
+    return voters, published, valid, parties
 
 
 def extract(web, header):
@@ -57,7 +57,7 @@ def extract(web, header):
     return int(info)
 
 
-def extract_stran(web):
+def extract_parties(web):
     soup = makeSoup(web)
     info = soup.find_all("td", class_="cislo", headers="t1sa2 t1sb3")
     info.extend(soup.find_all("td", class_="cislo", headers="t2sa2 t2sb3"))
@@ -66,7 +66,7 @@ def extract_stran(web):
             info[i] = int(info[i].text.replace("\xa0", ""))
     else:
         for cell in soup.find_all("td", class_="cislo", headers="s1"):
-            info_temp = extract_stran(urljoin(web, cell.a["href"]))
+            info_temp = extract_parties(urljoin(web, cell.a["href"]))
             if not info:
                 info = info_temp
             else:
@@ -78,13 +78,13 @@ def extract_stran(web):
 def header(web):
     header = ["Code", "Location", "Registered", "Envelopes", "Valid"]
     soup = makeSoup(web)
-    nazvy_stran = soup.find_all(
+    names_of_parties = soup.find_all(
         "td", class_="overflow_name", headers="t1sa1 t1sb2")
-    nazvy_stran.extend(soup.find_all(
+    names_of_parties.extend(soup.find_all(
         "td", class_="overflow_name", headers="t2sa1 t2sb2"))
-    for i in range(len(nazvy_stran)):
-        nazvy_stran[i] = nazvy_stran[i].text
-    return header.extend(nazvy_stran)
+    for i in range(len(names_of_parties)):
+        names_of_parties[i] = names_of_parties[i].text
+    return names_of_parties
 
 
 if __name__ == "__main__":
@@ -94,7 +94,7 @@ if __name__ == "__main__":
         web = sys.argv[1]
         outputFile = sys.argv[2]
         
-    except IndexError:
+    except:
         print("Not enough system arguments")
         exit()
     if not web.startswith("http"):
@@ -106,4 +106,4 @@ if __name__ == "__main__":
 
     with open(outputFile, "w", newline="", encoding='utf-8') as ofile:
         writer = csv.writer(ofile)
-        table, head = okresy(web, writer)
+        table, head = districts(web, writer)
